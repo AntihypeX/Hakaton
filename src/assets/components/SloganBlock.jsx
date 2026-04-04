e3644939cf8b00136f25e9ca7afeb919e874936f
Vilka42 <valerijkriger3@gmail.com>
2026-04-04 13:33:23 +0700

    why2

--- src/assets/components/SloganBlock.jsx
+++ src/assets/components/SloganBlock.jsx
@@ -3,8 +3,8 @@ function SloganBlock(){
     const toChatPage = useNavigate();
     return(
         <main className="main-block">
-            <h1 className="slogan">Слоган</h1>
-            <h2 className="slogan-under">Буковки какие то</h2>
+            <h1 className="slogan">Твой успех — наш приоритет: построим трек мечты!</h1>
+            <h2 className="slogan-under">Мы поможем составить план, прокачать навыки и найти работу, которая вдохновляет.</h2>
             <button className="go-to-chath" onClick={() => toChatPage('/chat')}>Зайти в чат</button>
         </main>
     )
