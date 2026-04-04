8b81d538f210381a1de1e80145118b23fc24d86b
Vilka42 <valerijkriger3@gmail.com>
2026-04-05 04:22:31 +0700

    gotovchik

--- src/assets/components/SloganBlock.jsx
+++ src/assets/components/SloganBlock.jsx
@@ -8,7 +8,7 @@ function SloganBlock(){
         <main className="main-block">
             <h1 className="slogan">Твой успех — наш приоритет: построим трек мечты!</h1>
             <h2 className="slogan-under">Мы поможем составить план, прокачать навыки и найти работу, которая вдохновляет</h2>
-            <button className="go-to-chath" onClick={handleChat}>Зайти в чат</button>
+            <button className="go-to-chath" onClick={handleChat}>Найти роботу</button>
         </main>
     )
 }
--- src/assets/components/UserPage/UserBlock.jsx
+++ src/assets/components/UserPage/UserBlock.jsx
@@ -17,43 +17,53 @@ function UserBlock(){
         window.location.href = '/';    
     }
     return(
-        <div className="userBlock">
-            <h2 className="user-title"> Настройка аккаунта </h2>
-                <div className="user-container">
-                    <div className="avatar-circle">
-                        {user.avatar && <img src={user.avatar} alt={user.name} />}
-                    </div>
-                    <span className="avatar-change">Сменить аватар</span> 
-                </div>
-                <div className="info-area">
-                    <div className="info-row">
-                        <span className="info-label">Имя:</span>
-                        <span className="info-value">{user.name}</span>
-                        <button className="edit-button">Изменить</button>
-                    </div>
-                    <div className="info-row">
-                        <span className="info-label">Почта:</span>
-                        <span className="info-value">{user.email}</span>
-                        <button className="edit-button">Изменить</button>
-                    </div>
-                    <div className="info-row">
-                        <span className="info-label">Пароль:</span>
-                        <span className="info-value">{user.password}</span>
-                        <button className="edit-button">Изменить</button>
-                    </div>
-                </div>
-                <h2 className=""> Список запросов</h2>
-                <div className="request-list">
-                    {requests.map((request, index) => (
-                        <div key={index} className="request-item">
-                            {request}
+        <div className="userfon">
+            <div className="userBlock">
+                <h2 className="user-title"> Настройка аккаунта </h2>
+                    <div className="user-settings">
+                        <div className="user-container">
+                            <div className="avatar-circle">
+                                {user.avatar && <img src={user.avatar} alt={user.name} />}
+                            </div>
+                            <span className="avatar-change">Сменить аватар</span> 
                         </div>
-                    ))}
-                </div>
-                <div className="buttons">
-                    <button className="create-request-button">Создать запрос</button>  
-                    <button className="logout-button" onClick={handleLogout}>Выход</button>
-                </div>
+                        <div className="info-area">
+                            <div className="info-row">
+                                <div>
+                                    <span className="info-label">Имя:</span>
+                                    <span className="info-value">{user.name}</span>
+                                </div>
+                                <button className="edit-button">Изменить</button>
+                            </div>
+                            <div className="info-row">
+                                <div>
+                                    <span className="info-label">Почта:</span>
+                                    <span className="info-value">{user.email}</span>
+                                </div>
+                                <button className="edit-button">Изменить</button>
+                            </div>
+                            <div className="info-row">
+                                <div>
+                                    <span className="info-label">Пароль:</span>
+                                    <span className="info-value">{user.password}</span>
+                                </div>
+                                <button className="edit-button">Изменить</button>
+                            </div>
+                        </div>
+                    </div>
+                    <h2 className=""> Список запросов</h2>
+                    <div className="request-list">
+                        {requests.map((request, index) => (
+                            <div key={index} className="request-item">
+                                {request}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="buttons">
+                        <button className="create-request-button">Создать запрос</button>  
+                        <button className="logout-button" onClick={handleLogout}>Выход</button>
+                    </div>
+            </div>
         </div>
     )
 }
