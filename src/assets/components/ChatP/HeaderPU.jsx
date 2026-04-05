3f945e009f4a4dc9f4e410e152234762bb73de76
GorizontIDC <151389770+GorizontIDC@users.noreply.github.com>
2026-04-05 12:02:52 +0700

    update HEaderPU

--- src/assets/components/ChatP/HeaderPU.jsx
+++ src/assets/components/ChatP/HeaderPU.jsx
@@ -1,43 +1,35 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import logotype from '../../image/логотип 23 (1).png';
 import { PersonCircle } from 'react-bootstrap-icons';
-import  '../../../styles/glav.css';
-import ModalLogIn from '../ModalWindows/ModalLogIn.jsx';
-import { useNavigate} from 'react-router-dom';
-import { useState } from 'react';
-function HeaderPU(){
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import '../../../styles/glav.css';
+function Header() {
+    const [isAuth, setIsAuth] = useState(false);
     const navigate = useNavigate();
-    const [ isLoginOpen, setIsLoginOpen ] = useState(false);
-    const handleProfileClick = () =>{
+    useEffect(() => {
         const user = localStorage.getItem('user');
         if (user) {
-            navigate('/user');
-        }else{
-            setIsLoginOpen(true);
+            setIsAuth(true);
         }
-    }
-    return(
+    }, []);
+    const handleProfileClick = () => {
+        if (isAuth) {
+            navigate('/user'); 
+        }
+    };
+    return (
         <header className="header">
-            <>
-                <a href="/" className="logo-link">
-                    <img src={logotype}
-                        alt="hakaton"
-                        className="logo-images"
-                    />
-                </a>
-            </>
-            <>
-                <div className='userProfile'>
-                    <button className="profile-button" onClick={handleProfileClick}>
-                       <PersonCircle />
-                    </button>
-                </div>
-            </>
-            <>
-                <ModalLogIn 
-                isOpen = {isLoginOpen}
-                onClose = {() => setIsLoginOpen(false)}/>
-            </>
+            <Link to="/" className="logo-link">
+                <img src={logotype} alt="hakaton" className="logo-images" />
+            </Link>
+            <div className='userProfile'>
+                <button className="profile-button" onClick={handleProfileClick}>
+                    <PersonCircle />
+                </button>
+            </div>
         </header>
-    )
+    );
 }
-export default HeaderPU;
+
+export default Header;
