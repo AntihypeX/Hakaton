47cb06573ac443c3ad6f766961a2e80f200628ff
Vilka42 <valerijkriger3@gmail.com>
2026-04-05 10:32:47 +0700

    chat 3
    chat

--- src/assets/components/ChatP/Chat.jsx
+++ src/assets/components/ChatP/Chat.jsx
@@ -1,20 +1,25 @@
 import { useNavigate } from "react-router-dom";
-function Chat() {
-    function BackButton() {
-        const toMainPage = useNavigate();
-        return (
-            <button onClick={() => toMainPage('/')}>Назад</button>
-        )
-    }
+
+function BackButton() {
+    const navigate = useNavigate();
     return (
-        <div>
-            <BackButton />
+        <button className="back" onClick={() => navigate('/')}>
+            Назад
+        </button>
+    );
+}
+
+function Chat() {
+    return (
             <div className="container">
                 <div className="sidebar">
+                    <BackButton />
+                    
                     <h1>Ваши чаты</h1>
-                    <button type="submit" className="chat active" >Название чата 1</button>
+                    <button type="submit" className="chat active">Название чата 1</button>
                     <button type="submit" className="chat">Название чата 2</button>
                 </div>
+                
                 <div className="workspace">
                     <div className="messages">
                         <div className="user-message">
@@ -25,13 +30,12 @@ function Chat() {
                         </div>
                     </div>
                     <div className="input">
-                        <textarea type="text" name="message" id="message" className="message"></textarea>
+                        <textarea name="message" id="message" className="message"></textarea>
                         <button type="submit">Отправить</button>
-                        
                     </div>
                 </div>
             </div>
-        </div>
-    )
+    );
 }
+
 export default Chat;
