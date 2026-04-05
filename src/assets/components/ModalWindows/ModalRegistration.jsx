9a3386622fec2f81bbf9af84bcdaf3a13195a184
GorizontIDC <151389770+GorizontIDC@users.noreply.github.com>
2026-04-05 10:18:30 +0700

    aaaa

--- src/assets/components/ModalWindows/ModalRegistration.jsx
+++ src/assets/components/ModalWindows/ModalRegistration.jsx
@@ -1,43 +1,54 @@
-import { toast } from 'react-toastify';
-import api from '../../../data/api.js'
-function ModalRegistr({ isOpen, onClose, onSwitch, setIsAuth }) {
-    const handleSubmit = async (e) => {
+import {toast} from 'react-toastify';
+import api from '../../../data/api.js';
+function ModalRegistr({isOpen, onClose, setIsAuth}){ 
+    if(!isOpen) return null;
+    const handleRegister = async (e) => {
         e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    
-    try {
-        await api.register({
-            login: formData.get('login'), 
-            password: formData.get('password'),
-            password2: formData.get('password2')
-        });
 
-        localStorage.setItem('user', 'true');
-        toast.success("Регистрация успешна!");
-        if (setIsAuth) setIsAuth(true);
-        onClose();
-    } catch (error) {
-        toast.error(error.message);
-    }
-};
-    if (!isOpen) return null;
+        const formData = new FormData(e.currentTarget);
+        const login = formData.get('login');
+        const password = formData.get('password');
+        const password2 = formData.get('password2');
+
+        try {
+            await api.register(login, password, password2);
+            localStorage.setItem('user', 'true');
+            toast.success("Вы успешно зарегистрировались!");
+            
+            if (setIsAuth) setIsAuth(true);
+            onClose();
+        } catch (error) {
+
+            toast.error(error.message || "Ошибка при регистрации");
+        }
+    };
     return (
-        <div className="modal-login-container" onClick={onClose}>
-            <div className="modal-login-container-items" onClick={(e) => e.stopPropagation()}>
-                <div className="modal-login-title">
-                    <h2>Авторизация</h2>
+        <div className="modal-registr-container" onClick={onClose}>
+            <div className="modal-registr-container-items" onClick={(e) => e.stopPropagation()}>
+                <div className="modal-registr-title">
+                    <h2>Регистрация</h2>
                 </div>
-                <div className="modal-login-form">
-                    <form className="login-form" onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Логин" id="login" name="login" required />
-                        <input type="password" placeholder="Пароль" id="password" name="password" required />
-                        <button type="submit" id="login-button" className="btn-log">
-                            Войти
-                        </button>
-                        <p>Нет аккаунта?</p>
-                        <span onClick={onSwitch} style={{ cursor: 'pointer', color: 'blue' }}>
-                            Зарегистрируйтесь
-                        </span>
+                <div className="modal-regisre-form">
+                    <form className="registr-form" onSubmit={handleRegister}>
+                        <input 
+                            type="text" 
+                            placeholder="Логин" 
+                            name="login" 
+                            required
+                        />
+                        <input 
+                            type="password" 
+                            placeholder="Пароль" 
+                            name="password" 
+                            required
+                        />
+                        <input 
+                            type="password" 
+                            placeholder="Повторите пароль" 
+                            name="password2"  
+                            required
+                        />
+                        <button type="submit" className="btn-log">Зарегистрироваться</button>
                     </form>
                 </div>
                 <button onClick={onClose} className="close-btn-log">Закрыть</button>
@@ -45,5 +56,4 @@ function ModalRegistr({ isOpen, onClose, onSwitch, setIsAuth }) {
         </div>
     );
 }
-
 export default ModalRegistr;
